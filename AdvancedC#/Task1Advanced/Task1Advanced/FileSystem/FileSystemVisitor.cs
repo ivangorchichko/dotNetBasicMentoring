deb88a767c2c4babc8c5710268d55dc365df50d3
Ivan_Harchychko <Ivan_Harchychko@epam.com>
2021-12-17 23:29:04 +0300

    Added main logic for Advanced

---
+++ AdvancedC#/Task1Advanced/Task1Advanced/FileSystem/FileSystemVisitor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task1Advanced.FileSystem
+{
+    public class FileSystemVisitor
+    {
+        private readonly string _root;
+        private DirectoryInfo _mainDirectoryInfo;
+        private IEnumerable<FileSystemObject> _folderObjects;
+        private readonly Predicate<string> _filter = (x) => true;
+        public FileSystemVisitor(string root)
+        {
+            if (root == null && !Directory.Exists(root))
+            {
+                throw new ArgumentNullException("File path does not exist!");
+            }
+            _root = root;
+            _mainDirectoryInfo = new DirectoryInfo(_root);
+            _folderObjects = new List<FileSystemObject>();
+        }
+
+        public FileSystemVisitor(string root, Predicate<string> algorithm)
+        {
+            _folderObjects = new List<FileSystemObject>();
+            _root = root;
+            _filter = algorithm;
+        }
+
+        public IEnumerable<FileSystemObject> GetDirectoryComponents()
+        {
+            foreach (var component in GetFoldersInDirectory(_root))
+            {
+                yield return component;
+            }
+        }
+
+        private IEnumerable<FileSystemObject> GetFoldersInDirectory(string root)
+        {
+            _mainDirectoryInfo = new DirectoryInfo(root);
+            yield return new FileSystemObject()
+            {
+                Name = _mainDirectoryInfo.Name,
+                ParentDirectoryPath = _mainDirectoryInfo.Parent.FullName,
+                Type = "Directory",
+            };
+
+            foreach (var subFolder in Directory.EnumerateDirectories(root))
+            {
+                foreach (var item in GetFoldersInDirectory(subFolder))
+                {
+                    yield return item;
+                }
+            }
+
+            foreach (var file in GetFilesInDictionary(root))
+            {
+                yield return file;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("----------end of folder------------ }\n");
+        }
+
+        private IEnumerable<FileSystemObject> GetFilesInDictionary(string folder)
+        {
+            _mainDirectoryInfo = new DirectoryInfo(folder);
+            foreach (var file in _mainDirectoryInfo.EnumerateFiles())
+            {
+                yield return new FileSystemObject()
+                {
+                    Name = file.Name,
+                    ParentDirectoryPath = file.DirectoryName,
+                    Type = file.Extension,
+                };
+            }
+        }
+    }
+}
---
+++ AdvancedC#/Task1Advanced/Task1Advanced/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Task1Advanced.FileSystem;
+
+namespace Task1Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path : ");
+            var path = Console.ReadLine();
+
+            var fileVisitor = new FileSystemVisitor(path);
+            var objectsInDirectory = fileVisitor.GetDirectoryComponents();
+
+            foreach (var directoryObject in objectsInDirectory)
+            {
+                if (directoryObject.Type != "Directory")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(directoryObject.ToString());
+            }
+
+        }
+    }
+}
